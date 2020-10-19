8691184b27bbf320106b48844023af9fa4aba6f7
kimnanhee <kimnanhee0225@gmail.com>
2020-10-19 10:21:49 +0900

    [Modify] maze_main.c

--- maze_main.c
+++ maze_main.c
@@ -6,20 +6,20 @@ int stack_point = 0;
 
 void push(int x, int y)
 {
-	printf("push - %d %d - %d\n", x, y, stack_point);
+	// printf("push - %d %d - %d\n", x, y, stack_point);
 	stack[0][stack_point] = x;
 	stack[1][stack_point] = y;
 	stack_point++;
 }
 void pop()
 {
-	printf("pop - %d\n", stack_point);
+	// printf("pop - %d\n", stack_point);
 	stack_point--;
 }
 int top()
 {
 	int top_value = stack[0][stack_point - 1] * 100 + stack[1][stack_point - 1];
-	printf("return value - %d\n", top_value);
+	// printf("return value - %d\n", top_value);
 	return top_value;
 }
 int main()
@@ -29,9 +29,10 @@ int main()
 	int cur_r, cur_c;
 
 	printf("배열을 입력해주세요\n");
-	for (int row = 0; row < miro_size; row++)
+	int row, col;
+	for (row = 0; row < miro_size; row++)
 	{
-		for (int col = 0; col < miro_size; col++)
+		for (col = 0; col < miro_size; col++)
 		{
 			int input;
 			scanf_s("%d", &input);
@@ -48,11 +49,9 @@ int main()
 	cur_c = s_Col;
 	push(cur_r, cur_c); // 시작 위치를 stack에 삽입
 
-	int cnt = 0;
 	while (1) 
 	{
-		printf("\n\n%d     (%d, %d)\n", cnt++, cur_r, cur_c);
-		miro[cur_r][cur_c] = 8;
+		miro[cur_r][cur_c] = 8; // 방문 했을때 
 
 		if (miro[cur_r][cur_c + 1] == 0 && top() != (cur_r * 100 + (cur_c + 1))) // 오른쪽이 뚫려있을 경우
 		{
@@ -78,10 +77,28 @@ int main()
 			cur_r = cur_r - 1;
 		}
 
-		else if (miro[cur_r][cur_c + 1] == 3 || miro[cur_r][cur_c - 1] == 3 || miro[cur_r + 1][cur_c] == 3 || miro[cur_r - 1][cur_c] == 3) 
+		else if (miro[cur_r][cur_c + 1] == 3) // right
 		{
 			push(cur_r, cur_c);
-			printf("finish\n");
+			push(cur_r, cur_c + 1);
+			break; 
+		}
+		else if (miro[cur_r][cur_c - 1] == 3) // left
+		{
+			push(cur_r, cur_c);
+			push(cur_r, cur_c - 1);
+			break; 
+		}
+		else if(miro[cur_r + 1][cur_c] == 3) // down
+		{
+			push(cur_r, cur_c);
+			push(cur_r + 1, cur_c);
+			break; 
+		}
+		else if (miro[cur_r - 1][cur_c] == 3) // up
+		{
+			push(cur_r, cur_c);
+			push(cur_r - 1, cur_c);
 			break; 
 		}
 		else 
@@ -97,8 +114,8 @@ int main()
 		}
 	}
 	int i = 0;
-	printf("---------------------\n");
-	for (i = 0; i < stack_point; i++) // stack에 들어있는 죄표 출력
+	printf("---------------------\nDirections\n\n");
+	for (i = 0; i < stack_point; i++) // stack에 들어있는 경로 출력
 	{
 		printf("%d - %d %d\n", i, stack[0][i], stack[1][i]);
 	}
